e7ac15f9d1e4baf175578f5486dd5b6c6d70f34c
pahels <pahel.srivastava@gmail.com>
2020-01-03 17:43:20 -0800

    parking autonomous programs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/Waterfall/PartialAuto/WaterfallParkingLane2Red.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.OpModes.Autonomous.Waterfall.PartialAuto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.ExtendedLinearOpMode;
+import org.firstinspires.ftc.teamcode.Hardware.Direction;
+import org.firstinspires.ftc.teamcode.Hardware.RobotVersion;
+
+@Autonomous(name = "Fall Parking Lane 1", group = "Waterfall")
+public class WaterfallParkingLane2Red extends ExtendedLinearOpMode {
+
+    public void runOpMode() {
+
+        robot.setHardwareMap(hardwareMap);
+        robot.initRiver(RobotVersion.RIVER);
+        initDetector();
+        //robot.initIMU();
+        //initIMU(hardwareMap);
+
+        telemetry.addLine("Ready to go!");
+        telemetry.update();
+
+        waitForStart();
+
+        //sleep until partner is done with tasks
+        //subject to change
+        sleep(1500);
+
+        //move backwards using encoders
+        encoderDrive(3, 3, .7, .7, 6);
+    }
+}
