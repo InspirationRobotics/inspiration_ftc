46308655d797464d30cfb248231ab661522e8931
Aditya Mavalankar <adityamavalankar@Adityas-MBP.local>
2020-01-10 21:56:53 -0800

    Foundation auto programs without use of distance sensors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/Waterfall/PartialAuto/WaterfallFoundationMoverBlueNoDSLane2.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.OpModes.Autonomous.Waterfall.PartialAuto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.ExtendedLinearOpMode;
+
+@Autonomous(name = "WaterfallFoundationMoverBlueNoDSLane2", group = "Waterfall")
+public class WaterfallFoundationMoverBlueNoDSLane2 extends ExtendedLinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        robot.setHardwareMap(hardwareMap);
+        robot.initWaterfall();
+        initIMU(hardwareMap);
+
+        telemetry.addLine("Ready to go!");
+        telemetry.update();
+
+        waitForStart();
+
+        encoderDrive(-30,-30,0.7,0.7,4);
+
+        encoderDrive(-5,-5,0.2,0.2,2);
+
+        robot.foundationServo.setPosition(robot.constants.FOUNDATION_SERVO_GRAB_POS);
+
+        sleep(1000);
+
+        encoderDrive(16,16, 0.4,0.4,3);
+
+        encoderDrive(6,15, 0.4,0.4,3);
+
+        gyroTurn(-90,0.7, 3);
+
+        encoderDrive(-24,-24,0.8,0.8,5);
+
+        robot.foundationServo.setPosition(robot.constants.FOUNDATION_SERVO_OPEN_POS);
+
+        sleep(500);
+
+        encoderDrive(5,5,0.6,0.6,2);
+
+        long endTime = System.currentTimeMillis() + 1500;
+
+        while (System.currentTimeMillis() < endTime && opModeIsActive()) {
+            strafeNoAngle(-1);
+        }
+
+        gyroTurn(-90, 0.7, 2);
+
+        encoderDrive(42,42,0.6,0.6,5);
+    }
+}
