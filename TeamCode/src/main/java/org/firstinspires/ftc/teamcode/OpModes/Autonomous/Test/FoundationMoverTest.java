ad482a72791dc33cbf58dcbe391a44d31430f309
pahels <pahel.srivastava@gmail.com>
2020-02-16 19:34:39 -0800

    foundation mover

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/Test/FoundationMoverTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/Test/FoundationMoverTest.java
@@ -4,10 +4,10 @@ package org.firstinspires.ftc.teamcode.OpModes.Autonomous.Test;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import org.firstinspires.ftc.teamcode.ExtendedLinearOpMode;
+import org.firstinspires.ftc.teamcode.Hardware.AllianceSide;
 import org.firstinspires.ftc.teamcode.Hardware.Direction;
 
 
-@Disabled
 @Autonomous(name = "Foundation Mover Test", group = "Test")
 public class FoundationMoverTest extends ExtendedLinearOpMode {
 
@@ -23,24 +23,7 @@ public class FoundationMoverTest extends ExtendedLinearOpMode {
 
         waitForStart();
 
-        gyroTurn(-90, 0.3, 3.5);
-
-        encoderDrive(-5, -5, 0.8,0.8,1.5);
-
-        robot.leftFoundation.setPosition(robot.constants.LEFT_FOUNDATION_GRAB_POS);
-        robot.rightFoundation.setPosition(robot.constants.RIGHT_FOUNDATION_GRAB_POS);
-
-        gyroTurn(-180, 0.3, 5);
-
-        robot.leftFoundation.setPosition(robot.constants.LEFT_FOUNDATION_OPEN_POS);
-        robot.rightFoundation.setPosition(robot.constants.RIGHT_FOUNDATION_OPEN_POS);
-
-        encoderDrive(5, 5, 0.8,0.8,1.5);
-
-        strafeDistSensor(26, Direction.LEFT, robot.distanceRight, 4000);
-
-        encoderDrive(55, 55, 0.6,0.6,1.5);
-
+        moveFoundation(AllianceSide.BLUE);
 
     }
 }
