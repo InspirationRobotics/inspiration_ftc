782c8bb2b42583880f037a98553725b6e4c3cb4a
RishiVeerepalli <rishi.veerepalli@gmail.com>
2020-02-19 21:16:32 -0800

    test for movetofoundation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/Regionals/Test/MoveToFoundation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/Regionals/Test/MoveToFoundation.java
@@ -1,4 +1,37 @@
 package org.firstinspires.ftc.teamcode.OpModes.Autonomous.Regionals.Test;
 
-public class MoveToFoundation {
+import com.inspiration.inspcv.CameraViewDisplay;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.BasicExtendedLinearOpMode;
+import org.firstinspires.ftc.teamcode.ExtendedLinearOpMode;
+import org.firstinspires.ftc.teamcode.Hardware.AllianceSide;
+import org.firstinspires.ftc.teamcode.Hardware.SkystonePosition;
+
+@Autonomous(name = "MoveToFoundationUnitTest", group = "Test")
+public class MoveToFoundation extends BasicExtendedLinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        robot.setHardwareMap(hardwareMap);
+        robot.initWaterfall();
+        robot.initDistanceSensors();
+        initIMU(hardwareMap);
+
+        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        detector.enable();
+
+        robot.backClawCollect.setPosition(robot.constants.BACK_CLAW_COLLECT_MID);
+
+        telemetry.addLine("Initialized! Ready to go!");
+        telemetry.update();
+
+        waitForStart();
+
+        moveToFoundation(3, AllianceSide.BLUE);
+        multipleStones(3, AllianceSide.BLUE);
+
+    }
+
 }
