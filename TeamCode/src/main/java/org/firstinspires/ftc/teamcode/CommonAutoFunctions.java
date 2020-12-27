f81fd23ebec099a78ceb868e101f9366df388111
RishiVeerepalli <rishi.veerepalli@gmail.com>
2020-12-26 18:03:15 -0800

    fix name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommonAutoFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommonAutoFunctions.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public abstract class MovementHelperFunctions extends LinearOpMode {
+public abstract class CommonAutoFunctions extends LinearOpMode {
 
     Robot robot = new Robot();
     private ElapsedTime runtime = new ElapsedTime();
