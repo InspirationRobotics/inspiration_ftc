663e06e7e20340e232ad7b0b41cc02ebb78b9d01
pahels <pahel.srivastava@gmail.com>
2020-12-30 10:41:45 -0800

    added servo stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/usrtestarea/pahel/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/usrtestarea/pahel/teleop.java
@@ -6,6 +6,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+/*MECHANICAL PEOPLE (noah): i added two variables: SERVO_POSITION and SERVER_RETRACTED_POSITION
+(I don't know how effective the name is lol). Feel free to increase/decrease the variable values as you figure out what
+position you need the servos to go to (I just set them arbitrarily). The greater the number the more it turns basically
+ */
+
 @TeleOp(name = "TeleOp wb")
 public class teleop extends LinearOpMode {
 
