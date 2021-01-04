e6dfc57b7f4d6bc948aa4cc0ea899ed04234f658
RishiVeerepalli <rishi.veerepalli@gmail.com>
2021-01-03 17:46:30 -0800

    fix teleop (again) and autonomous without computer vision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/usrtestarea/pahel/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/usrtestarea/pahel/teleop.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.usrtestarea.pahel;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -15,7 +16,7 @@ position you need the servos to go to (I just set them arbitrarily). The greater
 the two buttons are gamepad2 a and b lmk if you want me to change it */
 
 @TeleOp(name = "TeleOp wb")
-public class teleop extends CommonAutoFunctions {
+public class teleop extends OpMode {
 
     Robot robot = new Robot();
     public static final double STRAFE_SPEED = 0.5;
@@ -23,74 +24,58 @@ public class teleop extends CommonAutoFunctions {
     public static final double SERVO_RETRACTED_POSITION = 0.2;
 
     @Override
-    public void runOpMode() {
-
+    public void init() {
         robot.setHardwareMap(hardwareMap);
         robot.initDrivetrain();
         robot.initAllServos();
         robot.initMiscMotors();
+    }
 
-        waitForStart();
+    @Override
+    public void loop() {
+        robot.frontLeft.setPower(-gamepad1.left_stick_y);
+        robot.frontRight.setPower(-gamepad1.right_stick_y);
+        robot.backLeft.setPower(-gamepad1.left_stick_y);
+        robot.backRight.setPower(-gamepad1.right_stick_y);
 
-        while (opModeIsActive()) {
-
-            robot.frontLeft.setPower(-gamepad1.left_stick_y);
-            robot.frontRight.setPower(-gamepad1.right_stick_y);
-            robot.backLeft.setPower(-gamepad1.left_stick_y);
-            robot.backRight.setPower(-gamepad1.right_stick_y);
-
-            while (gamepad1.dpad_left) {
-                robot.frontLeft.setPower(-STRAFE_SPEED);
-                robot.frontRight.setPower(STRAFE_SPEED);
-                robot.backLeft.setPower(STRAFE_SPEED);
-                robot.backRight.setPower(-STRAFE_SPEED);
-
-                if (!gamepad1.dpad_left) {
-                    break;
-                }
-            }
-            while (gamepad1.dpad_right) {
-                robot.frontLeft.setPower(STRAFE_SPEED);
-                robot.frontRight.setPower(-STRAFE_SPEED);
-                robot.backLeft.setPower(-STRAFE_SPEED);
-                robot.backRight.setPower(STRAFE_SPEED);
-
-                if (!gamepad1.dpad_right) {
-                    break;
-                }
-            }
-
-            if (gamepad1.y) {
-                robot.shooterOne.setPower(-1);
-            }
-            if (gamepad1.x) {
-                robot.shooterOne.setPower(0);
-            }
-
-            while (gamepad1.right_bumper) {
-                robot.collector.setPower(-1);
-                if (!gamepad1.right_bumper) {
-                    robot.collector.setPower(0);
-                    break;
-                }
-            }
-            while (gamepad1.left_bumper) {
-                robot.collector.setPower(1);
-                if (!gamepad1.right_bumper) {
-                    robot.collector.setPower(0);
-                    break;
-                }
-            }
-
-            if (gamepad1.a) {
-                robot.shooter.setPosition(SERVO_POSITION);
-            }
-            if (gamepad1.b) {
-                robot.shooter.setPosition(SERVO_RETRACTED_POSITION);
-            }
-
+        if (gamepad1.dpad_left && !gamepad1.dpad_right) {
+            robot.frontLeft.setPower(-STRAFE_SPEED);
+            robot.frontRight.setPower(STRAFE_SPEED);
+            robot.backLeft.setPower(STRAFE_SPEED);
+            robot.backRight.setPower(-STRAFE_SPEED);
+        } else if (!gamepad1.dpad_left && gamepad1.dpad_right) {
+            robot.frontLeft.setPower(STRAFE_SPEED);
+            robot.frontRight.setPower(-STRAFE_SPEED);
+            robot.backLeft.setPower(-STRAFE_SPEED);
+            robot.backRight.setPower(STRAFE_SPEED);
+        } else {
+            robot.frontLeft.setPower(0);
+            robot.frontRight.setPower(0);
+            robot.backLeft.setPower(0);
+            robot.backRight.setPower(0);
         }
 
-        idle();
+        if (gamepad1.y) {
+            robot.shooterOne.setPower(-1);
+        }
+        if (gamepad1.x) {
+            robot.shooterOne.setPower(0);
+        }
+
+        if(!gamepad1.left_bumper && gamepad1.right_bumper) {
+            robot.collector.setPower(-1);
+        } else if (!gamepad1.right_bumper && gamepad1.left_bumper) {
+            robot.collector.setPower(1);
+        } else if (!gamepad1.left_bumper && !gamepad1.right_bumper) {
+            robot.collector.setPower(0);
+        }
+
+        if (gamepad1.a) {
+            robot.shooter.setPosition(SERVO_POSITION);
+        }
+        if (gamepad1.b) {
+            robot.shooter.setPosition(SERVO_RETRACTED_POSITION);
+        }
+
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/usrtestarea/rishi/SimpleAuto.java
@@ -0,0 +1,198 @@
+package org.firstinspires.ftc.teamcode.usrtestarea.rishi;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.CommonAutoFunctions;
+import org.firstinspires.ftc.teamcode.Robot;
+
+@Autonomous(name="SimpleAuto", group="SimpleAuto")
+public class SimpleAuto extends CommonAutoFunctions {
+
+    /* Declare OpMode members. */
+    Robot robot   = new Robot();   // Use a Pushbot's hardware
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        /* init hw */
+        hwit();
+        robot.setHardwareMap(hardwareMap);
+        robot.initDrivetrain();
+        robot.initAllServos();
+        robot.initMiscMotors();
+
+        /* relative (inches) to the corner closest to the starting position
+            x, y, angle to turn at the end
+        */
+        int[][] dropzonePositions = {
+                {24, 80, 60},
+                {24, 104, -60},
+                {24, 128, 60}
+        };
+        /* a: 0; b: 1; c: 2 */
+        int dropzoneNum = 0;
+
+        int[][] ringsPositions = {
+                {56, 72},
+                {62, 72},
+                {70, 72},
+        };
+
+        int parkY = 84;
+
+        telemetry.addData("Status", "ready");
+        telemetry.update();
+        waitForStart();
+
+        /* wobble goal */
+        encoderDriveByInchesDuplicate(dropzonePositions[dropzoneNum][1] - 18, dropzonePositions[dropzoneNum][1] - 18, 0.5, 5,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+        encoderTurnDuplicate(dropzonePositions[dropzoneNum][2], 0.5, 5,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+        encoderTurnDuplicate(-dropzonePositions[dropzoneNum][2], 0.5, 5,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+
+        /* ring 1 */
+
+        encoderDriveByInchesDuplicate(ringsPositions[0][1] - dropzonePositions[dropzoneNum][1],
+                ringsPositions[0][1] - dropzonePositions[dropzoneNum][1],
+                0.5,
+                8,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+        encoderTurnDuplicate(-90, 0.5, 5,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+        encoderDriveByInchesDuplicate(ringsPositions[0][0] - dropzonePositions[dropzoneNum][0],
+                ringsPositions[0][0] - dropzonePositions[dropzoneNum][0],
+                0.5,
+                8,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+        encoderTurnDuplicate(90, 0.5, 5,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+
+        robot.shooterOne.setPower(-1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addLine("r1");
+            telemetry.update();
+        }
+        robot.shooter.setPosition(0.2);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addLine("r1");
+            telemetry.update();
+        }
+        robot.shooter.setPosition(0.6);
+        robot.shooterOne.setPower(0);
+
+        /* ring 2 */
+        encoderTurnDuplicate(-90, 0.5, 5,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+        encoderDriveByInchesDuplicate(ringsPositions[1][0] - ringsPositions[0][0],
+                ringsPositions[1][0] - ringsPositions[0][0],
+                0.5,
+                8,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+        encoderTurnDuplicate(90, 0.5, 5,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+
+        robot.shooterOne.setPower(-1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addLine("r2");
+            telemetry.update();
+        }
+        robot.shooter.setPosition(0.2);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addLine("r1");
+            telemetry.update();
+        }
+        robot.shooter.setPosition(0.6);
+        robot.shooterOne.setPower(0);
+
+        /* ring 3 */
+        encoderTurnDuplicate(-90, 0.5, 5,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+        encoderDriveByInchesDuplicate(ringsPositions[2][0] - ringsPositions[1][0],
+                ringsPositions[1][0] - ringsPositions[0][0],
+                0.5,
+                8,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+        encoderTurnDuplicate(90, 0.5, 5,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+
+        robot.shooterOne.setPower(-1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addLine("r3");
+            telemetry.update();
+        }
+        robot.shooter.setPosition(0.2);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addLine("r1");
+            telemetry.update();
+        }
+        robot.shooter.setPosition(0.6);
+        robot.shooterOne.setPower(0);
+
+        /* park */
+
+        encoderDriveByInchesDuplicate(parkY - ringsPositions[2][1],
+                parkY - ringsPositions[2][1],
+                0.5,
+                8,
+                robot.frontLeft.getCurrentPosition(),
+                robot.frontRight.getCurrentPosition(),
+                robot.backLeft.getCurrentPosition(),
+                robot.backRight.getCurrentPosition());
+
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addLine("p");
+            telemetry.update();
+        }
+
+    }
+}
