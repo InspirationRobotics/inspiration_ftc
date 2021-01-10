93977fce89bc1e0f6c2a2a509b3747ae6bab2ae7
amavalankar <adima0704@gmail.com>
2021-01-09 17:41:06 -0800

    update SDK for OpenCV

---
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qualcomm.ftcrobotcontroller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 38;
+  public static final String VERSION_NAME = "6.0";
+  // Fields from default config.
+  public static final String BUILD_TIME = "2021-01-09T17:37:18.716-0800";
+}
