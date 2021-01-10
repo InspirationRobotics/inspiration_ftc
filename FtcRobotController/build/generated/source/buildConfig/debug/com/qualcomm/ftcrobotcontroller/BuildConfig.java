af58075d00343dd59e39df9e02c0288ba764a200
amavalankar <adima0704@gmail.com>
2021-01-10 13:21:04 -0800

    Make new branch to revert. Will send pull request if stable

---
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qualcomm.ftcrobotcontroller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 38;
+  public static final String VERSION_NAME = "6.0";
+  // Fields from default config.
+  public static final String BUILD_TIME = "2021-01-10T11:11:15.765-0800";
+}
